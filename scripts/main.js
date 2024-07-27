99149f21720ed4757fc27296c5c77cf00f2fecdc
YuraBuryakov <yuraburyakov@gmail.com>
2024-07-27 15:04:43 +0200

    added dialog window

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,18 @@ console.log("Hello World! This code runs immediately when the file is loaded.");
 
 Hooks.on("init", function() {
   console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
+
+  new Dialog({
+    title: "Hello World",
+    content: "<p>Hello, World!</p>",
+    buttons: {
+      ok: {
+        label: "OK",
+        callback: () => console.log("Dialog OK clicked!")
+      }
+    },
+    default: "ok"
+  }).render(true);
 });
 
 
