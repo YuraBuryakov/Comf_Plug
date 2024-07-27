af22952e2a82eb184c2a9895d6fa70095c93a4d8
YuraBuryakov <yuraburyakov@gmail.com>
2024-07-27 14:40:17 +0200

    added css

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,17 @@ Hooks.on("init", function() {
   console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
 });
 
+
 Hooks.on("ready", function() {
   console.log("This code runs once core initialization is ready and game data is available.");
+});
+
+Hooks.on("renderSidebarTab", function(app, html, data) {
+    if (app.options.id === "settings") {
+      html.find('.settings-directory').append('<button id="my-module-button">My Button</button>');
+    }
+  });
+
+$(document).on("click", "#my-module-button", function() {
+  console.log("Button Clicked!");
 });
